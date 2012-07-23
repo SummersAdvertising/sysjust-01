c8a1fe098b8206f7753193c660314e93f79a33fc
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-07-23 15:41:16 +0800

    add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120723055856) do
+ActiveRecord::Schema.define(:version => 20120723073615) do
 
   create_table "posts", :force => true do |t|
     t.text     "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120723055856) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "is_admin",               :default => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
