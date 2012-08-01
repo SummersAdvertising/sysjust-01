cc776c47aaee4151865afc9e79659e02b8a40bc3
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-01 11:05:52 +0800

    add a user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-	before_filter :prevent_sign_up, only: [:new]
+	# before_filter :prevent_sign_up, only: [:new]
 
-	private
-		def prevent_sign_up
-		redirect_to new_user_session_path and return
-	end
+	# private
+	# 	def prevent_sign_up
+	# 	redirect_to new_user_session_path and return
+	# end
 end
