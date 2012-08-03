64f783d00e9077d583775f4cda55f7a2eeb7d639
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-03 09:58:30 +0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.new
+admin_user.email = "admin@sysjust.com.tw"
+admin_user.password = "123456"
+admin_user.password_confirmation = "123456"
+admin_user.is_admin = true
+admin_user.save!
+
+user = User.new
+user.email = "user@sysjust.com.tw"
+user.password = "123456"
+user.password_confirmation = "123456"
+user.save!
