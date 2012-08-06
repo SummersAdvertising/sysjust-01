98fc13e55a0d3d03f1043bcf2d83fffccdcf039c
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-06 14:31:47 +0800

    check positive number for experience attentant number

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,5 @@
 class Experience < ActiveRecord::Base
-  attr_accessible :attendant_number, :company_name, :contact_person, :email, :phone_number, :professional_title, :comment
+	attr_accessible :attendant_number, :company_name, :contact_person, :email, :phone_number, :professional_title, :comment
+
+	validates :attendant_number, :numericality => { :greater_than => 0 }
 end
