96812cfc5170871a4c50ea197e83d3c4a84d6920
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-06 15:09:33 +0800

    validation-for-experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,13 @@
 class Experience < ActiveRecord::Base
 	attr_accessible :attendant_number, :company_name, :contact_person, :email, :phone_number, :professional_title, :comment
 
-	validates :attendant_number, :numericality => { :greater_than => 0 }
+	validates :attendant_number, :company_name, :contact_person, :email, :phone_number, :professional_title, :presence => true
+
+	validates :attendant_number, :numericality => { :greater_than => 0 }	
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX }    
+
+    VALID_PHONE_REGEX = /^[\d]+(\.[\d]+){0,1}$/	
+    validates :phone_number, :numericality => { :only_integer => true, :message => "Only number allowed" }    
 end
