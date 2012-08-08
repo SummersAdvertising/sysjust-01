f89a8fc2353753abb34bb25ab487b9330db0820a
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-08 11:28:35 +0800

    validation enrollment

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,11 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :course
   attr_accessible :company, :email, :name, :phone, :professional_title
+  
+  validates :company, :email, :name, :phone, :professional_title, :presence => true
+  
+  validates :phone, :numericality => { :only_integer => true, :message => "Only number allowed" }    
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX } 
 end
