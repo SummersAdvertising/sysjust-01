77d376d3db5fb682d6c3d439a8e5486134414cb8
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-08 10:40:25 +0800

    course has_many :enrollments, :dependent => :destroy

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
 class Course < ActiveRecord::Base
   attr_accessible :address, :content, :end_time, :is_display, :lecturer, :registration_quota, :session, :start_time, :subject
-  has_many :enrollments
+  has_many :enrollments, :dependent => :destroy
 end
