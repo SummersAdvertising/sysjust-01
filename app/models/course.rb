d63a4c02bd2fd2908577f0791e2570392bd7747a
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-20 17:36:56 +0800

    default order for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,14 @@
 class Course < ActiveRecord::Base
   attr_accessible :address, :content, :end_time, :is_display, :lecturer, :registration_quota, :session, :start_time, :subject
   has_many :enrollments, :dependent => :destroy
-  
+
   validates :address, :content, :end_time, :lecturer, :registration_quota, :session, :start_time, :subject, :presence => true
 
-  validates :registration_quota, :numericality => { :greater_than => 0 }  
+  validates :registration_quota, :numericality => { :greater_than => 0 }
 
   scope :recent, :order => 'updated_at DESC'
   scope :display, where(:is_display => true)
   scope :not_display, where(:is_display => false)
+  default_scope :order => 'created_at DESC'
   # scope :expire, where((:start_time[6..9].to_i * 365 * 24 * 60 + :start_time[0..1].to_i * 30 * 24 * 60 + :start_time[3..4].to_i * 24 * 60 + :start_time[11..12].to_i * 60 + :start_time[14..15].to_i)<(Time.now.year * 365 * 24 * 60 + Time.now.month * 30 * 24 * 60 + Time.now.day * 24 * 60 + Time.now.hour * 60 + Time.now.min))
 end
