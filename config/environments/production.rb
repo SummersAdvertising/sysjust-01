7fbae4cb2863759956711d9649bd1b0b2e1c4c24
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-30 11:53:00 +0800

    precompile reddactor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ TestDevise::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( rails.validations.js )
   config.assets.precompile += %w( ckeditor/* )
+  config.assets.precompile += %w( redactor-rails/* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
