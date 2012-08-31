dbd26ce22ae1fcb942df582c401cd31757861ca2
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-31 17:37:03 +0800

    validate length for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,5 +5,8 @@ class Video < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates :content, :title, :video, :image, :presence => true, :on => :create
   validates :content, :title, :presence => true, :on => :update
+
+  validates :content, :length => { :maximum => 66 }
+  validates :title, :length => { :maximum => 17 }
   default_scope :order => 'created_at DESC'
 end
