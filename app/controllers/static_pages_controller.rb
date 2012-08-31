12e88da5d82a7289ab0435af7c115e4444f65a15
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-31 18:02:10 +0800

    fix bug for home page to dispaly course

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -33,22 +33,21 @@ class StaticPagesController < ApplicationController
     current_month = Time.now.month
     current_day = Time.now.day
     current_year = Time.now.year
-    current_hour = Time.now.hour
-    current_minute = Time.now.min
-    current_time = current_year * 365 * 24 * 60 + current_month * 30 * 24 * 60 + current_day * 24 * 60 + current_hour * 60 + current_minute
+    current_time = current_year * 365 + current_month * 30 + current_day
 
     @courses = Course.display.start_time.recent
     @display_courses = [@courses[0], @courses[1], @courses[2]]
     i = 0
 
     @courses.each do |course|
-      month = course.start_time[0..1].to_i
-      day = course.start_time[3..4].to_i
-      year = course.start_time[6..9].to_i
-      hour = course.start_time[11..12].to_i
-      minute = course.start_time[14..15].to_i
-      time = year * 365 * 24 * 60 + month * 30 * 24 * 60 + day * 24 * 60 + hour * 60 + minute
-      if time > current_time
+      date_time = course.start_time.split(' ')
+      date = date_time[0]
+      date_split = date.split('/')
+      month = Integer(date_split[0].gsub("0", ""))
+      day = Integer(date_split[1].gsub("0", ""))
+      year = Integer(date_split[2])
+      temp_start_time = year * 365 + month * 30  + day
+      if temp_start_time > current_time
         @display_courses[i] = course
         i=i+1
       end
