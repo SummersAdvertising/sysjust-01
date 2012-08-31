e2e6663df438a730d3ce9f7e3b9d962742ffc78f
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-31 14:46:59 +0800

    validation banner title and content length

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -8,6 +8,8 @@ class Banner < ActiveRecord::Base
 
   validates :content_01, :content_02, :content_03, :hyperlink_01, :hyperlink_02, :hyperlink_03, :title_01, :title_02, :title_03, :excerpt_image_01, :excerpt_image_02, :excerpt_image_03, :presence => true, on: :create
   validates :content_01, :content_02, :content_03, :hyperlink_01, :hyperlink_02, :hyperlink_03, :title_01, :title_02, :title_03, :presence => true, on: :update
+  validates :content_01, :content_02, :content_03, :length => { :maximum => 24 }
+  validates :title_01, :title_02, :title_03, :length => { :maximum => 9 }
   scope :recent, :order => 'updated_at DESC'
 
 
