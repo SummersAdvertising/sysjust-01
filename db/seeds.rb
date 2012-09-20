a3fdd1747b4164c2f3a63d6f5cda2548d524b550
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-09-20 09:59:39 +0800

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 admin_user = User.new
+admin_user.name= "admin"
 admin_user.email = "admin@sysjust.com.tw"
 admin_user.password = "123456"
 admin_user.password_confirmation = "123456"
@@ -15,6 +16,7 @@ admin_user.is_admin = true
 admin_user.save!
 
 user = User.new
+user.name= "user"
 user.email = "user@sysjust.com.tw"
 user.password = "123456"
 user.password_confirmation = "123456"
