b786cadeb2d1bff54a0b1166b7703a0f42cd03b7
Yuzhe <yuzhe.c85@gmail.com>
2012-11-12 14:25:38 +0800

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TestDevise::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   
-  config.assets.precompile = [ method(:compile_asset?).to_proc ]
+  #config.assets.precompile = [ method(:compile_asset?).to_proc ]
   config.assets.precompile += %w( rails.validations.js )
   config.assets.precompile += %w( admin.js )
   config.assets.precompile += %w( redactor.js )
