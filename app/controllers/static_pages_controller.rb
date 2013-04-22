9b14918fd82030e860bd5f211ddfb093b4454150
Yuzhe <yuzhe.c85@gmail.com>
2013-04-22 12:26:40 +0800

    fix#2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -95,26 +95,31 @@ private
 		    remove_array = remove_colon.split(',array:')
 		    remove_array= remove_array[0]
 		    records = remove_array.split(',i')	    
-		    
+		    		    
 		    num.times { | i |
-			    record = records[i].split(':{')
-			    record = record[1]
-			    record = record.gsub("}", "")
-			    record_split = record.split(',')
-			    record_T1 = record_split[0]
-			    record_T1 = record_T1.gsub("T1:", "")
-			    record_T1 = record_T1.gsub("'", "")
-			    record_T2 = record_split[1]
-			    record_Title = record_split[2]
-			    record_Title = record_Title.gsub("Title:", "")
-			    record_Title = record_Title.gsub("'", "")
-			    record_Detail = record_split[3]
-			    record_Detail = record_Detail.gsub("Detail:", "")
-			    record_Detail = record_Detail.gsub("'", "")
-			    record_Detail = record_Detail.gsub("\n", "<br />")
-			    @record = Array[record_T1, record_T2, record_Title, record_Detail]
-			    
-			    @data.push(@record)
+		    	begin 
+				    record = records[i].split(':{')
+				    record = record[1]
+				    record = record.gsub("}", "")
+				    record_split = record.split(',')
+				    record_T1 = record_split[0]
+				    record_T1 = record_T1.gsub("T1:", "")
+				    record_T1 = record_T1.gsub("'", "")
+				    record_T2 = record_split[1]
+				    record_Title = record_split[2]
+				    record_Title = record_Title.gsub("Title:", "")
+				    record_Title = record_Title.gsub("'", "")
+				    record_Detail = record_split[3]
+				    record_Detail = record_Detail.gsub("Detail:", "")
+				    record_Detail = record_Detail.gsub("'", "")
+				    record_Detail = record_Detail.gsub("\n", "<br />")
+				    @record = Array[record_T1, record_T2, record_Title, record_Detail]    
+				    
+				rescue
+					next
+				else
+					@data.push(@record)
+				end
 		
 		    }
 		    
